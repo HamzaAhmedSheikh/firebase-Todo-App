80446d83d83435b34cf72b95cc0f9a172046b04a
HamzaAhmedSheikh <hamzaahmedsheikh313@gmail.com>
2020-08-14 21:54:05 +0500

    add data to firebase database

--- app.js
+++ app.js
@@ -1,1 +1,52 @@
 console.log('Firebase Todo App');
+
+  var list = document.getElementById('list')
+
+  function addTodo() {
+    var todo_item = document.getElementById('todo-item')
+
+    // create li tag with text node
+    var li = document.createElement('li')
+    var liText = document.createTextNode(todo_item.value)
+    var data = {
+        student: todo_item.value,
+    } 
+      
+      li.appendChild(liText)
+      
+     console.log(data);  
+
+     
+
+    // create delete button    
+    var delBtn = document.createElement("button")
+    var delText = document.createTextNode("DELETE")
+
+      delBtn.setAttribute("class", 'btn btn-primary btn-sm butt')
+      delBtn.setAttribute("onclick", "deleteItem(this)")
+      delBtn.appendChild(delText)
+
+    //create edit button 
+    var editBtn = document.createElement("button")
+    var editText = document.createTextNode("EDIT")  
+    
+       editBtn.setAttribute("class", 'btn btn-primary btn-sm butt')
+       editBtn.setAttribute("onclick", "editItem(this)")
+       editBtn.appendChild(editText)       
+
+    li.appendChild(delBtn)  
+    li.appendChild(editBtn)
+
+    list.appendChild(li)
+    list.setAttribute('class', 'list-group-item');
+
+    todo_item.value = ""
+
+    firebase.database().ref('todo/todoData').push(data)
+    
+}
+
+ function  
+
+
+       
